948a1f21d179725b6d5975a4eef310980bd55e93
Becky449 <rabnab12@gmail.com>
2023-03-29 23:41:49 +0300

    created db, tables, inserted information and queried them

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals ( 
+    id SERIAL NOT NULL , 
+    name text, date_of_birth date, 
+    escape_attempts integer, 
+    neutered boolean, 
+    weight_kg decimal 
+    );
